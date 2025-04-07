9622b29e31b96ccae86326864f487bc33f7df87e
sergi.martinez <smartinez@zitrointeractive.com>
2025-04-07 11:46:23 +0200

    first commit

---
+++ 01_print.py
@@ -0,0 +1,16 @@
+###
+# PRINT
+# El modulo print() se utiliza para mostrar información en la consola.
+# Sirve para mostrar mensajes, variables, resultados de operaciones, etc.
+###
+
+print("¡Hola Mundo!")
+print('Esto tambien funciona con una sola comilla')
+
+#Modificar se parador en cambio de un espacio
+print("Python", "es", "brutal", sep = "-")
+
+#Por defecto es un salto de linea, ahora me lo cambia a un espacio
+print("Python", "es", "brutal", end = " ")
+
+print(42)
---
+++ 02_types.py
@@ -0,0 +1,45 @@
+###
+# TYPES
+# Python tiene varios tipos de datos, entre los más comunes están:
+# - int: números enteros (ej: 42, -1, 0)
+# - float: números decimales (ej: 3.14, -0.5, 2.0)
+# - str: cadenas de texto (ej: "Hola", 'Python', "123")
+# - bool: booleanos (True o False)
+# - list: listas (ej: [1, 2, 3], ["a", "b", "c"])
+# - tuple: tuplas (ej: (1, 2, 3), ("a", "b", "c"))
+###
+"""
+asi puedes hacer una cadena de texto
+multilinea y si no lo asignas a nada...
+se trata de un comentario
+"""
+
+print("int:")
+# type: para saber que tipo de dato es el valor
+# print(type(42))  # int
+# print(type(-1))  # int
+
+print("float:")
+#print(type(3.14))  # float
+#print(type(1e3))  # float
+
+print("complex:")
+#print(type(1 + 2j))  # complex
+#print(type(1e3))  # complex
+
+print("str")
+#print(type("hola"))  # string
+#print(type("123"))  # string
+
+print("bool:")
+#print(type(True))  # bool
+#print(type(False))  # bool
+#print(type(1 < 2))  # bool
+
+print("bool:")
+#print(type(True))  # bool
+#print(type(False))  # bool
+#print(type(1 < 2))  # bool
+
+print("Nontype:")
+#print(type(None))  # NoneType
