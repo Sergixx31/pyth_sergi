d923e8d531d22bca8132f1c55bea9a9d3b42315d
sergi.martinez <smartinez@zitrointeractive.com>
2025-05-15 15:45:03 +0200

    casteo de entidades

---
+++ 03_cast.py
@@ -0,0 +1,18 @@
+###
+# CASTING DE TYPES
+# Transformar un tipo de un valor a otro
+###
+
+# print("Conversión de tipos")
+# print(type(int("100")))
+
+# print(2 + int("100")) #me transforma el texto 100 a numero y hace la suma
+# print("100" + str(2)) # me transforma el 2 a texto y lo concatena con el 100
+
+# print(type(float("3.1416")))
+# print(int(3.1416)) # me transforma el float a entero, pero me quita los decimales
+
+# print(bool(3))
+# print(bool(0)) # el unico que es false, es 0
+# print(bool(-1))
+# # print(bool("")) # cadena de texto vacia, por lo tanto es false
